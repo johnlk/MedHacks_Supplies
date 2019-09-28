b8cbc37853b27cf82b23a24e49e9dc554083295c
johnlk <jlkuhn@purdue.edu>
2019-09-28 14:28:50 -0400

    added functions for adding transactions and needsd

--- public/assets/js/database.js
+++ public/assets/js/database.js
@@ -33,11 +33,68 @@ function createProduct(productObj) {
   });
 }
 
+function createNeed(needObj){
+  return new Promise((resolve, reject) => {
+
+    if(!needObj.hasOwnProperty('productID') || !needObj.hasOwnProperty('quantity')
+      || !needObj.hasOwnProperty('hospitalID')){
+      reject('Needs require a productID, hospitalID, and a quantity');
+    }
+
+    database.collection("needs").add(needObj).then(() => {
+      resolve('success');
+    }).catch((error) => {
+      reject(error.message);
+    });
+
+  });
+}
+
+
+function completeTransaction(transactionObj){
+  return new Promise((resolve, reject) => {
+
+    if(!transactionObj.hasOwnProperty('donorID') || !transactionObj.hasOwnProperty('recieverID')
+      || !transactionObj.hasOwnProperty('needID')){
+      reject('Completed transactions require a donorID, recieverID, and a needID');
+    }
+
+    var needFulfilled = database.collection("needs").doc(transactionObj['needID']);
+    var pointsAwarded;
+
+    needFulfilled.get().then((querySnapshot) => {
+      var productID = querySnapshot.data().productID;
+      var quantity = querySnapshot.data().quantity;
+
+      pointsAwarded = computePoints(productID, quantity);
+
+      transactionObj['points'] = pointsAwarded;
+
+      database.collection("transactions").add(transactionObj).then(() => {
+        resolve('success');
+      }).catch((error) => {
+        reject(error.message);
+      });
+
+    }).catch((error) => {
+      reject(error.message);
+    });
+
+  });
+}
+
+function computePoints(productID, quantity){
+  return 100;
+}
 
 //on donation, or request for product, see if there is
 //already an existing match
 function searchForCompletedTransaction(){
 
+}
+
+function getTotalPoints(hospitalID){
+  
 }
 
 
