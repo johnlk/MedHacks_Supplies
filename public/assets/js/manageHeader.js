207e6eb8caa397e81aa2ae3b02591ec35854c744
greersmith97 <greersmith97@gmail.com>
2019-09-28 20:12:48 -0400

    added blank pages to complete header with sign out functionality

---
+++ public/assets/js/manageHeader.js
@@ -0,0 +1,10 @@
+isUserSignedIn().then((response) => {
+	var logOutBitch = document.getElementById("signInLogOut");
+	logOutBitch.innerHTML = "Log Out";
+	
+	logOutBitch.addEventListener("click", function(){
+		signOutUser().then((response) => {
+			document.location.reload();
+		})
+	})
+})
